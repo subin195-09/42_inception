a35b5da8c471614521beef7aa0253301c0122eee
Soobin Kim <skim@c8r8s12.42seoul.kr>
2021-09-20 19:27:11 +0900

    09/20 inception_env

---
+++ skim_test/srcs/requirements/mariadb/set_up.sh
@@ -0,0 +1,6 @@
+service mysql start;
+echo "CREATE DATABASE $WP_NAME;" | mysql;
+echo "CREATE USER '$WP_USER'@'%' IDENTIFIED BY '$WP_PWD';" | mysql;
+echo "GRANT ALL PRIVILEGES ON $WP_NAME.* TO '$WP_USER'@'%';" | mysql;
+echo "FLUSH PRIVILEGES;" | mysql;
+mysqld;
