86474ab9f670781314c005c2c6a687565926d048
subin195-09 <k_subin@naver.com>
2021-09-21 19:23:38 +0900

    09/21 wordpress database custom

---
+++ skim_test/srcs/requirements/mariadb/conf/wp_users.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO `wp_users` VALUES (1, 'skim', '1234', 'skim', 'k_subin@naver.com', 'https://localhost', '2021-09-09 00:00:00', '', 0, '수빈');
+INSERT INTO `wp_users` VALUES (2, 'skim2', '1234', 'skim2', 'k_subin@naver.com', 'https://localhost', '2021-09-09 00:00:00', '', 0, '수빈2');
