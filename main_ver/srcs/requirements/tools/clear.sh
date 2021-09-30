700e0ef177175ba2ad23a5abb9f7c9713bc20bb3
subin195-09 <k_subin@naver.com>
2021-09-30 18:32:08 +0900

    09/30 volume device 설정

--- main_ver/srcs/requirements/tools/clear.sh
+++ main_ver/srcs/requirements/tools/clear.sh
@@ -1,2 +1,5 @@
-docker rmi $(docker images -q)
-docker volume prune -f
+docker stop $(docker ps -qa);
+docker rm $(docker ps -qa);
+docker rmi -f $(docker images -qa);
+docker volume rm $(docker volume ls -q);
+docker network rm $(docker network ls -q) 2>/dev/null;
