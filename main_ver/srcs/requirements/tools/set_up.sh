0799ebb15fd45a39d8696f17c117b116ba3a947f
Soobin Kim <skim@c10r4s8.42seoul.kr>
2021-10-12 18:52:21 +0900

    10/12 vb setting add

--- main_ver/srcs/requirements/tools/set_up.sh
+++ main_ver/srcs/requirements/tools/set_up.sh
@@ -4,4 +4,6 @@
 apt-get install docker -y
 apt-get install docker.compose -y
 apt-get install make -y
+mkdir /home/skim/wordpress
+mkdir /home/skim/mariadb
 cp ./srcs/requirements/tools/hosts /etc/hosts
