a8d76af4fb118a2b22992fc7f75a29b628e8980b
Soobin Kim <skim@c10r4s8.42seoul.kr>
2021-10-12 17:30:27 +0900

    10/12 vb setting add

--- main_ver/srcs/requirements/tools/set_up.sh
+++ main_ver/srcs/requirements/tools/set_up.sh
@@ -1,7 +1,7 @@
 # apt-get upgrade
 # apt-get update
 # apt-get git -y
-apt-get docker -y
-apt-get docker.compose -y
-apt-get make -y
+apt-get install docker -y
+apt-get install docker.compose -y
+apt-get install make -y
 cp hosts /etc/hosts
